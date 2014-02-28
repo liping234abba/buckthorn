2b31be55b81a6016c4671728a3ebbc6d59479c5a
wave <sea_buckthorn@126.com>
2014-02-28 22:55:35 +0800

    Test

--- Measure/src/com/wave/mzpad/test/TestAndroid.java
+++ Measure/src/com/wave/mzpad/test/TestAndroid.java
@@ -1,11 +1,19 @@
 package com.wave.mzpad.test;
 
+import jxl.WorkbookSettings;
 import android.test.AndroidTestCase;
 
 public class TestAndroid extends AndroidTestCase {
 
 	public void testSelect(){
 		System.out.println();
+	}
+	
+	
+	public void testWorkSheet(){
+		
+		WorkbookSettings workbook = new WorkbookSettings();
+		
 	}
 	
 }
